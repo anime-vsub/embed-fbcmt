af31e121bb03898f2e527f6dfd8988ec9899b9f9
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-01-12 19:05:48 +0700

    try not

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
     entry: ["src/client/main.ts", "src/client/vue.ts"],
-    outDir: "dist",
+    outDir: "dist-client",
     dts: true,
     treeshake: true,
     clean: true,
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,19 @@
 import path from "path"
 
 import vue from "@vitejs/plugin-vue"
-import AutoImport from "unplugin-auto-import/vite"
 import { defineConfig } from "vite"
 import WindiCSS from "vite-plugin-windicss"
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
-    WindiCSS(),
-    AutoImport({
-      imports: [
-        "vue",
-        "vue-router",
-        "@vueuse/core",
-        {
-          "@vueuse/router": ["useRouteQuery"],
-        },
-      ],
-    }),
+    WindiCSS()
   ],
   resolve: {
     alias: {
       "src": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "./dist-web"
   }
 })
