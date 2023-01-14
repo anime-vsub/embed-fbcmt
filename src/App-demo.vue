393b9bc07b310d448ee03a895cb7b2e3c1f241b6
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-01-14 19:23:37 +0700

    demo

---
+++ src/App-demo.vue
@@ -0,0 +1,7 @@
+<template>
+  <EmbedFbCmt service="http://localhost:5173" href="https://google.com" />
+</template>
+
+<script lang="ts" setup>
+import { EmbedFbCmt } from "./client/vue"
+</script>
---
+++ src/main-demo.ts
@@ -0,0 +1,8 @@
+import "virtual:windi.css"
+import "virtual:windi-devtools"
+
+import { createApp } from "vue"
+
+import App from "./App-demo.vue"
+
+createApp(App).mount("#app")
